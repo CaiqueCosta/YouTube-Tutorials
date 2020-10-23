b511ca40c1f49a33edc65e512e9ecf4d67427785
caiquecosta <ccosta22@live.com>
2020-10-22 23:23:01 -0400

    Added connection to cassandra

--- CandyShop/cmd/main.go
+++ CandyShop/cmd/main.go
@@ -6,9 +6,22 @@ import (
 	"net/http"
 
 	"github.com/CaiqueCosta/YouTube-Tutorials/CandyShop/pkg/http/rest"
+	"github.com/CaiqueCosta/YouTube-Tutorials/CandyShop/pkg/storage"
 )
 
 func main() {
+
+	r, err := storage.SetupStorage()
+	if err != nil {
+		log.Fatalln("error while setting up storage:", err)
+	}
+
+	c, err := r.GetAllCandyNames()
+	if err != nil {
+		log.Fatalln("error while getting candies in storage:", err)
+	}
+	log.Println(c)
+
 	fmt.Println("starting server on port 8080...")
 	router := rest.InitHandlers()
 	log.Fatal(http.ListenAndServe(":8080", router))
--- CandyShop/go.mod
+++ CandyShop/go.mod
@@ -2,4 +2,7 @@ module github.com/CaiqueCosta/YouTube-Tutorials/CandyShop
 
 go 1.14
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gocql/gocql v0.0.0-20200926162733-393f0c961220
+	github.com/gorilla/mux v1.8.0
+)
