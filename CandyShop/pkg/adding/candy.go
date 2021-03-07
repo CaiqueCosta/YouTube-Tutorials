21cf64c8b05da3f19d21c513fbc43a3f1603298f
MarErm27 <v27i72@gmail.com>
2021-03-07 15:45:04 +0300

    changing the type of the price from int back to float 32

--- CandyShop/pkg/adding/candy.go
+++ CandyShop/pkg/adding/candy.go
@@ -4,5 +4,5 @@ type Candy struct {
 	Id       string `json:"id",omitempty`
 	Category string `json:"category"`
 	Name     string `json:"name"`
-	Price    int    `json:"price"`
+	Price    float32    `json:"price"`
 }
